321d4a4189bbd18c017c1f1796e8a3a2eab77dac
Jérôme Genzling <111453682+JGenzling@users.noreply.github.com>
2025-05-28 15:43:35 -0400

    Add files via upload

--- GNN/predict.py
+++ GNN/predict.py
@@ -11,7 +11,7 @@
 # pip install rdkit
 # pip install seaborn
 # pip install hyperopt
-# To vizualize smiles, https://www.cheminfo.org/flavor/malaria/Utilities/SMILES_generator___checker/index.html
+# To vizualize smiles, you may use: https://www.cheminfo.org/flavor/malaria/Utilities/SMILES_generator___checker/index.html
 
 import torch
 import numpy as np
@@ -20,18 +20,21 @@ import os
 import time
 import pandas as pd
 import copy
-from rdkit import Chem
-from rdkit.Chem import AllChem
+import faulthandler
+
+#faulthandler.enable()
+from GNN import GNN
 from torch_geometric.loader import DataLoader
 from tqdm import tqdm
-
-from GNN import GNN, GNN_New
-from prepare_set import generate_infersets, dump_datasets
-from transfer_chirality import transfer_chirality, process_transfer_chirality_in_batches
-from utils import calculate_metrics, load_data
+#from hyperoptimize import hyperoptimize
+from plot_and_print import print_model_txt
+from prepare_set import generate_datasets, dump_datasets
 from argParser import argsParser
-from utils import find_protonation_state
-
+from utils import set_cuda_visible_device, load_data, find_protonation_state
+from train_pKa_predictor import training, testing, inferring, testing_with_IC, infer
+from usage import usage
+from rdkit import Chem
+from rdkit.Chem import Descriptors
 
 def set_seed(args, seed=42):
     torch.manual_seed(args.seed)
@@ -106,7 +109,9 @@ def predict(csv_file, device=None, data_path=r'../Datasets/', mode='pH', pH = 7.
     library_infer_ionization_states = []
 
     for i, small_mol in tqdm(data.iterrows(), total=len(data)):
-        # initial_proposed_center = int(small_mol['Index']) + 1
+        #initial_proposed_center = int(small_mol['Index']) + 1
+        if Descriptors.ExactMolWt(Chem.MolFromSmiles(small_mol['Smiles']))>= 1000:
+            raise ValueError(f"You're trying to predict the pKa of a species that seems too big for a small molecule (>1000 g/mol): {small_mol['Smiles']}")
         ionized_smiles = ''
         initial = True
         infer_predicts, infer_labels, infer_smiles, infer_smiles_base, infer_mol_num, infer_centers, \
@@ -262,223 +267,5 @@ def predict(csv_file, device=None, data_path=r'../Datasets/', mode='pH', pH = 7.
     return results_pka_pred, results_smiles_pred
 
 
-def infer(i, small_mol, initial, ionized_smiles, ionization_states, infer_path, model_params, device, best_hypers, loss_fn, args):
-
-    infer_dataset, ionized_smiles = generate_infersets(small_mol, i, initial, ionized_smiles, ionization_states, args)
-    dump_datasets(infer_dataset, infer_path)
-
-    # If no acid/base pair found, we exit
-    if len(infer_dataset) == 0:
-        infer_predicts = []
-        infer_labels = []
-        infer_smiles = []
-        infer_smiles_base = []
-        infer_mol_num = []
-        infer_centers = []
-        infer_proposed_centers = []
-        infer_neutrals = []
-        infer_ionization_states = []
-        return infer_predicts, infer_labels, infer_smiles, infer_smiles_base, infer_mol_num, \
-            infer_centers, infer_proposed_centers, infer_neutrals, infer_ionization_states, ionized_smiles
-
-    # Loading data for training
-    infer_data = load_data(args.infer_pickled)
-
-    if args.GATv2Conv_Or_Other == "GATv2Conv":
-        model_infer = GNN(feature_size=infer_dataset[0].x.shape[1],
-                          edge_dim=infer_dataset[0].edge_attr.shape[1],
-                          model_params=model_params)
-    else:
-        model_infer = GNN_New(feature_size=infer_dataset[0].x.shape[1],
-                          edge_dim=infer_dataset[0].edge_attr.shape[1],
-                          model_params=model_params)
-
-    checkpoint = torch.load(args.model_dir + args.model_name, map_location=torch.device('cpu'), weights_only=True)
-    model_infer.load_state_dict(checkpoint['model_state_dict'])
-    model_infer.eval()
-
-    infer_loader = DataLoader(infer_data, best_hypers["batch_size"],
-                              num_workers=0, shuffle=False)
-
-    infer_loss, infer_predicts, infer_labels, infer_smiles, infer_smiles_base, infer_centers, infer_proposed_centers,\
-        infer_mol_num, infer_neutral, infer_error, infer_ionization_states = \
-        final_test(model=model_infer, loader=infer_loader, loss_fn=loss_fn, args=args)
-
-    return infer_predicts, infer_labels, infer_smiles, infer_smiles_base, infer_mol_num, infer_centers, \
-        infer_proposed_centers, infer_neutral, infer_ionization_states, ionized_smiles
-
-
-
-def final_test(loader, model, loss_fn, args):
-    device = torch.device("cuda:0" if torch.cuda.is_available() and args.mode == 'train' else "cpu")
-
-    # Enumerate over the data
-    all_preds = []
-    all_labels = []
-    running_loss = 0.0
-    step = 0
-    all_centers = []
-    all_proposed_centers = []
-    all_smiles = []
-    all_smiles_base = []
-    all_mol_num = []
-    all_neutral = []
-    all_error = []
-    all_ionization_states = []
-    model.p = 0
-    for batch in loader:
-        batch.to(device)
-
-        with torch.no_grad():  # turns of the autograd engine. Less memory and faster
-
-            pred = model(batch.x.float(),
-                         batch.edge_index,
-                         batch.edge_attr.float(),
-                         batch.node_index,
-                         batch.mol_formal_charge,
-                         batch.center_formal_charge,
-                         batch.batch)
-
-        if pred.size()[0] > 1:
-            loss = loss_fn(torch.squeeze(pred), batch.y.float())
-            all_preds.append(torch.squeeze(pred).cpu().detach().numpy())
-        else:
-            loss = loss_fn(pred[0], batch.y.float())
-            all_preds.append(pred[0].cpu().detach().numpy())
-
-        running_loss += loss.item()
-        step += 1
-        all_smiles.append(batch.smiles)
-        all_smiles_base.append(batch.smiles_base)
-        all_neutral.append(batch.neutral.cpu().detach().numpy())
-        all_labels.append(batch.y.cpu().detach().numpy())
-        all_centers.append(batch.ionization_center.cpu().detach().numpy())
-        all_proposed_centers.append(batch.proposed_center.cpu().detach().numpy())
-        all_mol_num.append(batch.mol_number.cpu().detach().numpy())
-        all_error.append(batch.error.cpu().detach().numpy())
-        all_ionization_states.append(batch.ionization_state)
-
-    if len(all_preds) > 0 and all_preds[0].size > 1:
-        all_preds = np.concatenate(all_preds).ravel()
-        all_labels = np.concatenate(all_labels).ravel()
-        all_centers = np.concatenate(all_centers).ravel()
-        all_proposed_centers = np.concatenate(all_proposed_centers).ravel()
-        all_smiles = np.concatenate(all_smiles).ravel()
-        all_smiles_base = np.concatenate(all_smiles_base).ravel()
-        all_neutral = np.concatenate(all_neutral).ravel()
-        all_mol_num = np.concatenate(all_mol_num).ravel()
-        all_error = np.concatenate(all_error).ravel()
-    elif len(all_preds) > 0:
-        all_preds = np.array(all_preds[0]).ravel()
-        all_labels = np.array(all_labels[0]).ravel()
-        all_centers = np.array(all_centers[0]).ravel()
-        all_proposed_centers = np.array(all_proposed_centers[0]).ravel()
-        all_smiles = np.array(all_smiles[0]).ravel()
-        all_smiles_base = np.array(all_smiles_base[0]).ravel()
-        all_neutral = np.array(all_neutral[0]).ravel()
-        all_mol_num = np.array(all_mol_num[0]).ravel()
-        all_error = np.array(all_error[0]).ravel()
-
-    if step == 0:
-        step = 1
-
-    if args.mode == 'train':
-        calculate_metrics(all_preds, all_labels, all_mol_num, args)
-
-    return running_loss / step, all_preds, all_labels, all_smiles, all_smiles_base, all_centers, all_proposed_centers,\
-        all_mol_num, all_neutral, all_error, all_ionization_states
-
-
-def export_sdf_rdkit(dataframe, output_file='molecules_prostates.sdf'):
-    """
-    Exports molecules from the 'Predicted pKa smiles' column of the DataFrame to a single SDF file.
-
-    Args:
-        dataframe (pd.DataFrame): DataFrame containing the 'Predicted pKa smiles' column with SMILES strings.
-        output_file (str): The filename for the output SDF file."""
-
-    smiles_column = "Predicted pKa smiles updated" if "Predicted pKa smiles updated" in dataframe.columns else "Predicted pKa smiles"
-    writer = Chem.SDWriter(output_file)
-    for idx, row in dataframe.iterrows():
-
-        smiles = row.get(smiles_column)
-        if not isinstance(smiles, str) or not smiles.strip():
-            print(f"Skipping row {idx}: invalid or missing SMILES string.")
-            continue
-
-        mol = Chem.MolFromSmiles(smiles)
-        if mol is None:
-            print(f"Could not parse SMILES on row {idx}: '{smiles}'")
-            continue
-
-        # Optionally, set a name or other properties to the molecule (will be included in the SDF)
-        mol.SetProp("_Name", str(row.get('Name')))
-
-        # Add missing (implicit) Hs as explicit Hs
-        mol_with_all_h = Chem.AddHs(mol, addCoords=True)
-
-        # Optionally generate coordinates (comment out if you don’t need them)
-        AllChem.EmbedMolecule(mol_with_all_h, randomSeed=0xf00d)
-
-        # Write to output SDF
-        writer.write(mol_with_all_h)
-
-    writer.close()
-    print(f"SDF file saved to '{output_file}'.")
-
-
 if __name__ == '__main__':
-
-    csv_path = r'C:\Users\Jerome Genzling\OneDrive - McGill University\Documents\Research\pKa predictor\ACIE Submission\pKaPredictorPipeline\Datasets\trbl.csv'
-    csv = pd.read_csv(csv_path)
-    a,b = predict(csv)
-    print('Results:')
-    #add results to csv
-    csv['Predicted pKa'] = a #This corresponds to the pKa value < pH that gave this protonation site
-    csv['Predicted pKa smiles'] = b
-
-    # # for smiles do transfer_chirality(orig_smiles, prot_smiles)
-    # for i in range(len(csv)):
-    #     orig_smiles = csv.iloc[i]['Smiles']
-    #     prot_smiles = csv.iloc[i]['Predicted pKa smiles']
-    #     if orig_smiles != prot_smiles:
-    #         new_smiles = transfer_chirality(orig_smiles, prot_smiles)
-    #         csv.loc[i, 'Predicted pKa smiles'] = new_smiles
-
-    updated_csv = process_transfer_chirality_in_batches(csv , batch_size=50)
-    print(csv)
-    updated_csv.to_csv(csv_path.replace('.csv' , '_updated.csv') , index=False)
-    export_sdf_rdkit(updated_csv, csv_path.replace('.csv' , '_updated.sdf'))
-
-
-
-    # smi_path = r"C:\Users\Jerome Genzling\OneDrive - McGill University\Documents\Research\pKa predictor\ACIE Submission\pKaPredictorPipeline\Datasets\missing_compounds.smi"
-    '''writer = Chem.SDWriter('test_fabio.sdf')
-    smi = pd.read_csv(smi_path, header=None)
-    smi.columns = ['Smiles']
-    for idx, row in smi.iterrows():
-        smiles = row.get('Smiles')
-        if '.' in smiles:
-            continue
-
-        if not isinstance(smiles, str) or not smiles.strip():
-            print(f"Skipping row {idx}: invalid or missing SMILES string.")
-            continue
-
-        mol = Chem.MolFromSmiles(smiles)
-        if mol is None:
-            print(f"Could not parse SMILES on row {idx}: '{smiles}'")
-            continue
-
-        # Optionally, set a name or other properties to the molecule (will be included in the SDF)
-        mol.SetProp("_Name", row.get('Name'))
-
-        # Add missing (implicit) Hs as explicit Hs
-        mol_with_all_h = Chem.AddHs(mol, addCoords=True)
-
-        # Optionally generate coordinates (comment out if you don’t need them)
-        AllChem.EmbedMolecule(mol_with_all_h, randomSeed=0xf00d)
-
-        # Write to output SDF
-        writer.write(mol_with_all_h)
-    writer.close()'''
+    a,b = predict('trbl.csv')
